62a5114137c48eb75d32071be1060e5233b8b7c1
AnnaEvich <alltidensom@gmail.com>
2023-05-01 23:41:17 +0300

    feat: add keycodes (Mon 01.05.2023 21:33:57)

---
+++ script.js
@@ -0,0 +1,31 @@
+let keyCodeList = [];
+
+const keyCodeListRu = [
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete',
+  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+  'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift',
+  'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight',
+];
+const keyCodeListEn = [
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+  'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', ',', '/', 'ArrowUp', 'Shift',
+  'Control', 'Meta', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight',
+];
+
+const specialKeyNameList = {
+  'Control': 'Ctrl',
+  'Delete': 'Del',
+  'CapsLock': 'Caps Lock',
+  ' ': 'Space',
+  'Meta': 'Win',
+  'ArrowRight': '→',
+  'ArrowLeft': '←',
+  'ArrowDown': '↓',
+  'ArrowUp': '↑',
+};
+document.addEventListener('DOMContentLoaded', function() {
+
+});
