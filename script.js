8ab01769684eb3c5c2e2bd031bd74c3ac24843ca
AnnaEvich <alltidensom@gmail.com>
2023-05-01 23:46:06 +0300

    feat: add creation of html blocks (Mon 01.05.2023 23:01:07)

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let keyCodeList = [];
-
 const keyCodeListRu = [
   '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete',
@@ -26,6 +24,45 @@ const specialKeyNameList = {
   'ArrowDown': '↓',
   'ArrowUp': '↑',
 };
-document.addEventListener('DOMContentLoaded', function() {
 
+// let keyCodeList = [];
+let keyCodeList = keyCodeListEn;
+
+
+function createBodyContainer() {
+  let createBody = document.createElement('main');
+  createBody.id = 'body-container';
+  return createBody;
+}
+
+function createKeyboardInput() {
+  let createInput = document.createElement('input');
+  createInput.id = 'keyboard-input';
+  return createInput;
+}
+
+function createKeys() {
+  let keysContainer = document.createElement('div');
+  keysContainer.id = 'keys-container';
+
+  for (let i = 0; i < keyCodeList.length; i++) {
+    let keyName = specialKeyNameList[keyCodeList[i]]? specialKeyNameList[keyCodeList[i]] : keyCodeList[i];
+    let key = document.createElement('div');
+    key.classList.add('key');
+    if (keyCodeList[i] === ' '){
+      key.classList.add('space');
+    }
+    key.id = keyCodeList[i];
+    key.innerText = keyName;
+    keysContainer.appendChild(key);
+  }
+
+  return keysContainer;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  let bodyContainer = createBodyContainer();
+  bodyContainer.appendChild(createKeyboardInput());
+  bodyContainer.appendChild(createKeys());
+  document.body.appendChild(bodyContainer);
 });
